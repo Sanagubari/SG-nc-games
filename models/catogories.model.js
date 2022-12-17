2dd4a4273c7f661a2bc8c97ef0679cab8fe68f42
Sana <sanagubari27@gmail.com>
2022-12-17 00:21:17 +0000

    Implement GET/api/users/:username endpoint and test happy and sad paths

--- models/catogories.model.js
+++ models/catogories.model.js
@@ -165,3 +165,17 @@ exports.checkCategoryExists = (category) => {
     return true;
   }
 };
+
+exports.selectSpecificUser = (username) => {
+  return db
+    .query(`SELECT * FROM users WHERE username =$1`, [username])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `Not found: Username '${username}' does not exist`,
+        });
+      }
+      return rows[0];
+    });
+};
